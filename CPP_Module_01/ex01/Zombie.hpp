4767a6c2605cd2f0ef31ec5dbaf5d1114d055194
Elnop <elnop@dev.fr>
2023-10-29 00:02:51 +0200

    Brainzzzzzz

--- CPP_Module_01/ex01/Zombie.hpp
+++ CPP_Module_01/ex01/Zombie.hpp
@@ -3,10 +3,14 @@
 # include <iostream>
 
 class Zombie {
+	private:
+		std::string	name;
 	public:
 		Zombie(std::string name);
 		~Zombie();
-		void announce(void);
+		void	announce(void);
 };
 
+Zombie* newZombie( std::string name );
+void 	randomChump( std::string name );
 #endif
